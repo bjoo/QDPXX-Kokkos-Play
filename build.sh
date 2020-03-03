fb066ab910aeb6ea14ca816770ac5095916afb25
Balint Joo <bjoo@jlab.org>
2020-03-03 16:16:37 -0500

    Added Eclipse magic

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ cmake \
 	-DKokkos_ENABLE_OPENMP=ON \
 	-DCMAKE_CXX_COMPILER=${CXX} \
 	-DCMAKE_CXX_FLAGS="-g -O3" \
+	-G"Eclipse CDT4 - Unix Makefiles" \
+    	-DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8 \
+    	-DCMAKE_ECLIPSE_VERSION=4.5.0 \
 	..
 
 make -j 8
