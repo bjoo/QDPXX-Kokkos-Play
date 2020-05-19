3205685a56ba044f5e3116abdccc0d3381a844d3
Balint Joo <bjoo@jlab.org>
2020-05-19 10:20:21 -0500

    Got rid of unused command line argument warning which broke gtest builds....

--- scripts/build_hip_tulip.sh
+++ scripts/build_hip_tulip.sh
@@ -20,10 +20,11 @@ cmake \
 	-DKokkos_ENABLE_SERIAL=ON \
 	-DKokkos_ENABLE_HIP=ON \
 	-DKokkos_ARCH_VEGA906=ON \
+	-DKokkos_ENABLE_COMPLEX_ALIGN=ON \
 	-DCMAKE_CXX_COMPILER=${CXX} \
-	-DCMAKE_CXX_FLAGS="-O3" \
+	-DCMAKE_CXX_FLAGS="-O3 --amdgpu-target=gfx906 -Wno-error=unused-command-line-argument" \
 	-DBUILD_GMOCK=OFF \
-	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_BUILD_TYPE=Debug \
 	${SRCDIR}/QDPXX-Kokkos-Play
 
 
